ad03e0d08d7facdec8aae1ce2da2a391f42f0891
nijesa <nisaza570@gmail.com>
2025-09-29 09:42:19 -0500

    FUNCIONA GH-PAGES

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Footer from './component/footer/footer'
-import { Toaster } from 'react-hot-toast'
 import Ejemplo from './pages/Ejemplo'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import Botonin from './component/footer/Botonin'
 import NotFound from './pages/notFound'
 import Menu from './component/menu/menu'
 import RecuperarCuenta from './pages/recuperarCuenta'
 import RecetaComp from './component/Receta/RecetaComp'
-import RecetasPg from './pages/RecetasPg'
+import RecetaPg from './pages/RecetasPg'
 import PerfilPg from './pages/PerfilPg'
-import RecetasGuardadas from './pages/RecetasGuardadas'
+import { Toaster } from 'react-hot-toast'
 
 
 
 createRoot(document.getElementById('root')).render(
-
-  <StrictMode>
-  <BrowserRouter>
-  <Menu/>
-  <Toaster />
+ <StrictMode>
+    <Toaster/>
+     <HashRouter>
         <Routes>
           <Route path="/" element ={<Home/>}/>
           <Route path="/inicioSesion" element ={<Ejemplo/>}/>
@@ -30,11 +27,9 @@ createRoot(document.getElementById('root')).render(
           <Route path='*' element={<NotFound/>}/>
           <Route path='/Recuperar_Contrasena' element={<RecuperarCuenta/>}/>
           <Route path='/RecetaComp' element={<RecetaComp/>}/>
-          <Route path='/RecetaPg' element={<RecetasPg/>}/>
-          <Route path='/RecetasGuardadas' element={<RecetasGuardadas/>}/>
+          <Route path='/RecetaPg' element={<RecetaPg/>}/>
           <Route path='/PerfilPg' element={<PerfilPg/>}/>
-          
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   </StrictMode>,
 )
